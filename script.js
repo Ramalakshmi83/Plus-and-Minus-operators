f07861141c380de3a96036f47f1fa602d29004a8
Ramalakshmi83 <ramagubbala92@gmail.com>
2024-12-20 22:21:20 +0530

    Add files via upload
    Add operators

---
+++ script.js
@@ -0,0 +1,11 @@
+function Decrease(){
+    let value=document.getElementById('counter').innerHTML
+    value--;
+    document.getElementById('counter').innerHTML=value
+
+}
+function Increase(){
+    let value=document.getElementById('counter').innerHTML
+    value++;
+    document.getElementById('counter').innerHTML=value
+}
